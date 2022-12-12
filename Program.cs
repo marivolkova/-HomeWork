a65943f8ccfa15c5f0d45bb3a4135a7ddd812e4f
Marina <mari7723@inbox.ru>
2022-12-12 12:14:19 +0300

    Home Work Seminar3 added

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ else
     min = num1;
 }
 Console.WriteLine($"Большее это: {max}");
-Console.WriteLine($"Меньшее это:{min}");
+Console.WriteLine($"Меньшее это: {min}");
 */
 
 
@@ -47,6 +47,7 @@ Console.WriteLine(max);
 /*
 Console.Write("Input an integer number: ");
 int num = Convert.ToInt32(Console.ReadLine());
+
 if  (num % 2 == 0)
 Console.Write("Yes");
 else
@@ -143,15 +144,80 @@ int Num = Convert.ToInt32(Console.ReadLine());
 Console.Write("Your day of week: " + dayNum);
 */
 
+// Семинар 3 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+/*
+    bool Palindrom(int num)
+{
+    int n1 = (num / 10000);
+    int n2 = ((num / 1000)%10);
+    int n4 = ((num % 100)-n1)/10;
+    int n5 = (num % 10);
 
-
-
-
-
-
-
-
-
-
- 
+    if (n1 == n5 && n2 == n4) return true;
     
+    else  return false;
+}
+
+Console.Write("Input a five-digies number: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+bool pal = Palindrom(Num);
+Console.Write("This number is " + pal);
+*/
+
+//Задача 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+/*
+double Distance (double xA, double yA,double zA, double xB, double yB, double zB)
+{  
+   double lenght = Math.Sqrt(Math.Pow(xB - xA,2)+ Math.Pow(yB-yA,2)+Math.Pow(zB-zA,2));
+   return Math.Round (lenght,2);
+}
+
+Console.Write("Input an x-coordinate of point A: ");
+double xA = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input an y-coordinate of point A: ");
+double yA = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input an z-coordinate of point A: ");
+double zA = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Input an x_coordinate of point B: ");
+double xB = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input an y_coordinate of point B: ");
+double yB = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input an z-coordinate of point B: ");
+double zB = Convert.ToDouble(Console.ReadLine());
+
+
+double distanceBetweenAB = Distance(xA, yA, zA, xB, yB, zB);
+
+Console.WriteLine($"Distance between the pointes A and B is {distanceBetweenAB} ");
+*/
+
+// Задача 3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*
+void Cubes(int number)
+{
+   int counter = 1;
+   while (counter <= number)
+
+   {
+      Console.Write($" {Math.Pow(counter,3)}");
+      if (counter != number) Console.Write (",");
+      counter++;
+   }
+}
+
+Console.Write("Input an integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("The series of a squares of numbers: ");
+Cubes(num);
+*/
+
+
+
